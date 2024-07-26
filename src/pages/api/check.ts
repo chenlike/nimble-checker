8133e2173674bb5c5d07fe11a6855a650e5bac38
clkclk <clkclk1997@foxmail.com>
2024-07-26 10:41:23 +0800

    add CAPTCHA

--- src/pages/api/check.ts
+++ src/pages/api/check.ts
@@ -1,24 +1,111 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 import type { NextApiRequest, NextApiResponse } from "next";
 import axios from "axios";
-type NimbleResData = {
-    msg: string;
-};
+import dotenv from "dotenv"
+dotenv.config()
 
-export default function handler(
+
+// NIMBLE_CAPTCHA_KEY
+
+async function checkBalance(address: string) {
+    try {
+        let res = await axios.post("https://mainnet.nimble.technology:443/check_balance", {
+            "address": address
+        })
+        if (res.status == 200) {
+            return {
+                address: address,
+                msg: res.data.msg
+            }
+        } else {
+            return {
+                address: address,
+                msg: res.data
+            }
+        }
+    } catch (e:any) {
+        return {
+            address: address,
+            msg: JSON.stringify(e.response.data)
+        }
+    }
+}
+
+export default async function handler(
     req: NextApiRequest,
-    res: NextApiResponse<NimbleResData>,
+    res: NextApiResponse,
 ) {
 
-    axios.post("https://mainnet.nimble.technology:443/check_balance", { 
-        "address":  req.body.address
-    }).then((response) => {
-        res.status(200).json(response.data);
-    }).catch((error) => {
-    
+    let captchaKey = process.env.NIMBLE_CAPTCHA_KEY
+    if (!captchaKey) {
         res.status(200).json({
-            msg:JSON.stringify(error.response.data)
+            msg: "Please solve the CAPTCHA first"
         });
+        return
+    }
 
-    });
+    if (!req.body.address) {
+        res.status(200).json({
+            msg: "Please input nimble address first"
+        });
+        return
+    }
+
+    const cfResponse = await axios.post(
+        'https://challenges.cloudflare.com/turnstile/v0/siteverify',
+        new URLSearchParams({
+            secret: captchaKey,
+            response: req.body.captcha,
+        }).toString(),
+        {
+            headers: {
+                'Content-Type': 'application/x-www-form-urlencoded',
+            },
+        }
+    );
+    console.log(cfResponse.data)
+    if (cfResponse.data.success === false) {
+        res.status(200).json({
+            msg: "Please retry the CAPTCHA "
+        });
+        return
+    }
+
+
+
+
+    let addresses = req.body.address.split("\n")
+    // 过滤空的地址和重复的地址
+    const reg = /^\s*$/;
+    addresses = addresses.filter((addr: string) => !reg.test(addr));
+    addresses = Array.from(new Set(addresses))
+    if (addresses.length === 0) {
+        res.status(200).json({
+            msg: "Please input nimble address first"
+        });
+        return;
+    }
+
+
+    // 并发请求接口
+    let promises:Promise<{ address:string,msg:string }>[] = []
+    for (let i = 0; i < addresses.length; i++) {
+        promises.push(checkBalance(addresses[i]))
+    }
+
+    await Promise.allSettled(promises)
+
+    let results = []
+
+    // 获得所有promise 的结果
+    for (let i = 0; i < promises.length; i++) {
+        results.push(await promises[i])
+    }
+
+
+
+
+    res.status(200).json(results);
+
+
 }
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,8 +2,17 @@ import Head from "next/head";
 import { ConfigProvider, theme } from "antd";
 import Checker from "./Checker";
 import { Analytics } from "@vercel/analytics/react"
-
+import { useEffect } from "react"
 export default function Home() {
+
+  useEffect(() => {
+    // 客户端相关代码在这里执行
+    const script = document.createElement('script');
+    script.src = "https://challenges.cloudflare.com/turnstile/v0/api.js";
+    script.defer = true;
+    document.body.appendChild(script);
+  }, []);
+
   return (
     <>
 
